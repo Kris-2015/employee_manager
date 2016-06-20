a9642102b4fcfbbc29c93734e8d28a37b625df0c
Kris-2015 <krishnadev.rnc@gmail.com>
2016-06-20 12:52:11 +0530

    change in display.php

--- display.php
+++ display.php
@@ -15,63 +15,66 @@ require('insert.php');
 <!DOCTYPE html>
 <html>
    <head>
-      <title>DISPLAY INFORMATION</title>
-      <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-      <link rel="stylesheet" type="text/css" href="css/display.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <title>DISPLAY INFORMATION</title>
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/display.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script src="js/jQuery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script> -->
+    <!-- <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
    </head>
    <body>
-      <h2 class="h2"><u>REGISTERED USERS</u></h2>
-      <div class="row">
-         <div class="col-xs-12">
-            <form method="POST">
-               <div class="table-responsive">
-                  <table class="table table-bordered table-hover">
-                     <thead>
-                        <tr>
-                           <td>Name</td>
-                           <td>Prefix</td>
-                           <td>Gender</td>
-                           <td>DOB</td>
-                           <td>Marital Status</td>
-                           <td>Employer</td>
-                           <td>Employment</td>
-                           <td>Picture</td>
-                           <td>Official Address</td>
-                           <td>Residential Address</td>
-                           <td>Communication</td>
-                           <td>Edit</td>
-                           <td>Delete</td>
-                        </tr>
-                     </thead>
-                     <tbody>
-                        <?php
-                           $conn = mysqli_connect($hostname, $username, $pass, $db_name);
+    <h2 class="h2"><u>REGISTERED USERS</u></h2>
+     <div class="row">
+      <div class="col-xs-12">
+        <form method="POST">
+          <div class="table-responsive">
+            <table class="table table-bordered table-hover">
+              <thead>
+                <tr>
+                  <td>Name</td>
+                  <td>Prefix</td>
+                  <td>Gender</td>
+                  <td>DOB</td>
+                  <td>Marital Status</td>
+                  <td>Employer</td>
+                  <td>Employment</td>
+                  <td>Picture</td>
+                  <td>Official Address</td>
+                  <td>Residential Address</td>
+                  <td>Communication</td>
+                  <td>Edit</td>
+                  <td>Delete</td>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+
+                    $conn = mysqli_connect($hostname, $username, $pass, $db_name);
+
+                    //Display operation
+             
+                    $fetch = " SELECT *, (
+                       select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addr WHERE type = 'office' AND addr.employee_id = e.id
+                        ) as official_address,
+                        (
+                          select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addrhome WHERE type = 'residence' AND addrhome.employee_id = e.id
+                        ) AS residential_address,
+                        (
+                          SELECT type FROM communication c
+                          WHERE c.employee_id = e.id
+                        ) AS communication_medium
+                        FROM employee e";
+                                  
+                        $result = mysqli_query($conn,$fetch);
                            
-                           //Display operation
-                           
-                           $fetch = " SELECT *, (
-                                    select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addr WHERE type = 'office' AND addr.employee_id = e.id
-                                     ) as official_address,
-                                    (
-                                 select GROUP_CONCAT(street, ' ', city, ' ', zip) AS official_address FROM address addrhome WHERE type = 'residence' AND addrhome.employee_id = e.id
-                                    ) AS residential_address,
-                                    (
-                                     SELECT type FROM communication c
-                                     WHERE c.employee_id = e.id
-                                    ) AS communication_medium
-                                    FROM employee e";
-                           
-                           
-                                   $result = mysqli_query($conn,$fetch);
-                           
-                               	if(mysqli_num_rows($result) > 0)
-                               	{
-                                   	//output of the data
-                                   	while($row = mysqli_fetch_assoc($result))
-                                   	{
-                           			$image = $row["image"];
+                        if(mysqli_num_rows($result) > 0)
+                         	{
+                           	//output of the data
+                           	while($row = mysqli_fetch_assoc($result))
+                           	{
+                          			$image = $row["image"];
 
                                 echo "<tr>";
                                 echo "<td>" . $row["first_name"]." ".$row["middle_name"]." ". $row["last_name"] ."</td>";
@@ -93,7 +96,7 @@ require('insert.php');
                                 echo "<td>" . $row["official_address"]  ."</td>";
                                 echo "<td>" . $row["residential_address"]  ."</td>";
                                 echo "<td>" . $row["communication_medium"] . "</td>";
-                                echo "<td>" . '<a href="registration.php/?&emp_id=' . $row['id'] . '&userAction=update">Edit</a></td>';
+                                echo "<td>" . '<a href="registration.php/?emp_id=' . $row['id'] . '&userAction=update">Edit</a></td>';
                                 echo "<td>" . '<a href="delete.php?emp_id=' . $row['id'] .'">Delete</a></td>';
                            
                                echo "</tr>";
@@ -103,15 +106,12 @@ require('insert.php');
                             {
                              	echo "<br>Insert Record";
                             }
-                           ?>
-                     </tbody>
-                  </table>
-               </div>
-            </form>
-         </div>
+                  ?>
+              </tbody>
+            </table>
+          </div>
+        </form>
       </div>
-   </body>
+    </div>
+  </body>
 </html>
-
-
-
