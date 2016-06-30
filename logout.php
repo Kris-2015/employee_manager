7d085187ea551564a97ba6ba21405ba5638376be
Kris-2015 <krishnadev.rnc@gmail.com>
2016-06-30 13:43:28 +0530

    added logout file

---
+++ logout.php
@@ -0,0 +1,21 @@
+	<?php
+
+	/*
+	 * @author: mfsi_krishnadev
+	 * @purpose: logging out the user
+	 * @access: public
+	*/
+
+	include("session_check.php");
+	session_start();
+	$obj = new session_check();
+	$logout = $obj->logout();
+
+	var_dump($logout);
+	
+	if($logout)
+	{
+		header("Location: login.php");
+	}
+
+?>
