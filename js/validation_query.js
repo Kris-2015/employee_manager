ec0baf6c60cf2da35415d9f8a61d2a0d60c02835
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-01 17:56:29 +0530

    added jquery validation

---
+++ js/validation_query.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$("#fname").focus(function(){
+		$("#error_firstname").css("display","initial");
+	});
+
+	$("#mname").focus(function(){
+         $("#error_middlename").css("display","initial");
+	});
+
+	$("#lname").focus(function(){
+
+	});
+});
