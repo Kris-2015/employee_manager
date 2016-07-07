29b0acb7beef8d1cedf3888010ec0d8e3efa844a
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-08 00:46:56 +0530

    changes in line

--- js/search.js
+++ js/search.js
@@ -39,42 +39,39 @@ $(document).ready(function()
 	});
 
 	$(".class_order").on("click", function(){
-		/*console.log(this);
-		return false;*/
-		$.ajax(
+		
+	  $.ajax(
+	  {
+		url:'../sort.php',
+		type:'POST',
+		dataType:'json',
+		data:
 		{
-			url:'../sort.php',
-			type:'POST',
-			dataType:'json',
-			data:
-			{
-			    name:$(this).attr("id"),
-			},
-			success:function(data)
-			{
-				console.log(data);
-			    var sort_data = "";
-			    for(var key in data)
-			    {
-			    	if(data.hasOwnProperty(key))
-			    	{
-			    		sort_data += "<tr>";
-			    			sort_data += "<td>" + data[key]["name"] + "</td>";
-			    			sort_data += "<td>" + data[key]["gender"] + "</td>";
-			    			sort_data += "<td>" + data[key]["dob"] + "</td>";
-			    			sort_data += "<td>" + data[key]["email_id"] + "</td>";
-			    			sort_data += "<td>" + data[key]["marital_status"] + "</td>";
-			    			sort_data += "<td>" + data[key]["office"] + "</td>";
-			    			sort_data += "<td>" + data[key]["residence"] + "</td>";
-			    			sort_data += "<td>" + data[key]["communication"] + "</td>";
-			    			sort_data += "<td>" + '<a href="/registration.php/?emp_id=' + data[key]["id"] +'&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href="/registration.php/?emp_id='+ data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +"</td>";
-			    		sort_data += "</tr>";
-			    	}
+		    name:$(this).attr("id"),
+		},
+		success:function(data)
+		{
+		    var sort_data = "";
+		    for(var key in data)
+		    {
+		    	if(data.hasOwnProperty(key))
+		    	{
+		    		sort_data += "<tr>";
+	    			    sort_data += "<td>" + data[key]["name"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["gender"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["dob"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["email_id"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["marital_status"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["office"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["residence"] + "</td>";
+	    			    sort_data += "<td>" + data[key]["communication"] + "</td>";
+	    			    sort_data += "<td>" + '<a href="/registration.php/?emp_id=' + data[key]["id"] +'&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href="/registration.php/?emp_id='+ data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +"</td>";
+		    		sort_data += "</tr>";
 			    }
-
-			    $("#tab tbody").html(sort_data);
 			}
+			    $("#tab tbody").html(sort_data);
+		}
 
-		});
 	});
+  });
 });
