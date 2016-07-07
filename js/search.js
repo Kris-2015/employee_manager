b0bb231ff81ce766aee54139ec82083787a60b7f
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-07 17:01:30 +0530

    addon of sort operation

--- js/search.js
+++ js/search.js
@@ -4,8 +4,9 @@ $(document).ready(function()
 	{	
 	  $.ajax(
 	  {
-	    url: '../search_result.php',
+	    url: '../test.php',
 		type: 'POST',
+		dataType: 'json',
 		data:
 		{
 		  name:$("#name").val(),
@@ -13,14 +14,64 @@ $(document).ready(function()
 		},
 		success:function(data)
 		{
-		   //console.log(data);
-		   $("#tab").html(data);
-		},
-		error:function(error)
-		{
-			$("#tab").text("Sorry, search result not found");
+           var arr_data = "";
+		   for(var key in data)
+		   {
+		   	if(data.hasOwnProperty(key))
+		   	{
+		   		arr_data += "<tr>";
+		   		    arr_data += "<td>" + data[key]["name"] + "</td>";
+		   		    arr_data += "<td>" + data[key]["gender"] + "</td>";
+		   		    arr_data += "<td>" + data[key]["dob"] + "</td>";
+		   		    arr_data += "<td>" + data[key]["email_id"] + "</td>";
+		   		    arr_data += "<td>" + data[key]["marital_status"] + "</td>";
+		   		    arr_data += "<td>" + data[key]["office"] + "</td>" ;
+		   		    arr_data += "<td>" + data[key]["residence"] + "</td>"; 
+		   		    arr_data += "<td>" + data[key]["communication"] + "</td>";
+		   		    arr_data += "<td>" + '<a href="/registration.php/?emp_id=' + data[key]["id"] +'&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href="/registration.php/?emp_id='+ data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +"</td>";
+		   		arr_data += "</tr>";
+		   	}
+		   }
+
+		   $("#tab tbody").html(arr_data);		   
 		}
-	  });
-		return false;
+		});
+	});
+
+	$(".class_order").on("click", function(){
+		console.log(this);
+
+		$.ajax(
+		{
+			url:'../sort.php',
+			type:'POST'
+			dataType:'json',
+			data:
+			{
+			    name: $(this).(".class-name").val();
+			}
+			success:function(data)
+			{
+			    var sort_data = "";
+			    for(var key in data)
+			    {
+			    	if(data.hasOwnProperty(key))
+			    	{
+			    		sort_data += "<tr>";
+			    			sort_data += "<td>" + data[key]["name"] + "</td>";
+			    			sort_data += "<td>" + data[key]["gender"] + "</td>";
+			    			sort_data += "<td>" + data[key]["dob"] + "</td>";
+			    			sort_data += "<td>" + data[key]["email_id"] + "</td>";
+			    			sort_data += "<td>" + data[key]["marital_status"] + "</td>";
+			    			sort_data += "<td>" + data[key]["office"] + "</td>";
+			    			sort_data += "<td>" + data[key]["residence"] + "</td>";
+			    			sort_data += "<td>" + data[key]["communication"] + "</td>";
+			    			sort_data += "<td>" + '<a href="/registration.php/?emp_id=' + data[key]["id"] +'&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href="/registration.php/?emp_id='+ data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' +"</td>";
+			    		sort_data += "</tr>";
+			    	}
+			    }
+			}
+
+		});
 	});
 });
