40ff7e272ef5154a44e6f58f04ce74231b8e0119
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-08 08:48:29 +0530

    modified user file

--- user.php
+++ user.php
@@ -15,23 +15,32 @@ class user extends db_connection
 	public function check($mail_id, $pass)
 	{
 		$email_id = $this->test_input($mail_id);
-		$password = md5($pass);
+		$password = $this->test_input($pass);
+		//$password = md5($pass);
 
-		$select = "SELECT id, CONCAT(first_name, ' ',last_name)as name 
-		  FROM employee
-		  WHERE email_id = '$email_id' 
-		  AND password = '$password'";
-
-		$query = mysqli_query($this->connect, $select);
-		$row = mysqli_num_rows($query);
-
-		if($row == 1)
+		if(empty($email_id) && empty($password))
 		{
-			session_start();
-			$row = mysqli_fetch_assoc($query); 		
-			$_SESSION['user_id'] = $row['id'];
-			$_SESSION['user_name'] = $row['name'];
-			return true;
+			return false;
+		}
+		else
+		{
+			$hash_password = md5($password);
+			$select = "SELECT id, CONCAT(first_name, ' ',last_name)as name 
+		  	FROM employee
+		  	WHERE email_id = '$email_id' 
+		  	AND password = '$hash_password'";
+
+			$query = mysqli_query($this->connect, $select);
+			$row = mysqli_num_rows($query);
+
+			if($row == 1)
+			{
+				session_start();
+				$row = mysqli_fetch_assoc($query); 		
+				$_SESSION['user_id'] = $row['id'];
+				$_SESSION['user_name'] = $row['name'];
+				return true;
+			}
 		}
 	}
 
