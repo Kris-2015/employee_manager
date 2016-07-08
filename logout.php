ab6fb4be789c59fd52c6a2e195ef638f66da7d3e
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-08 08:58:27 +0530

    modified logout file

--- logout.php
+++ logout.php
@@ -4,13 +4,16 @@
 	 * @purpose: logging out the user
 	 * @access: public
 	*/
+
+	//including the session check class file
 	include("session_check.php");
 	session_start();
+	//instantiating the object of session_check class
 	$obj = new session_check();
+	//calling the function logout to destroy session
 	$logout = $obj->logout();
-	
-	var_dump($logout);
-	
+
+	//checking if the result is true, then logout the user
 	if($logout)
 	{
 		header("Location: login.php");
