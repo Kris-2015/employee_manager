37350afc18937acc94bd5dfc444f3eae3a55670f
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-11 07:06:28 +0530

    change in sorting file

--- sort.php
+++ sort.php
@@ -1,29 +1,15 @@
 <?php
-    ini_set("display_error","1");
-    session_start();
-	  require("user.php");
+ini_set("display_error","1");
+session_start();
+require("DataFilter.php");
 
-	//accepting the value through ajax
-	
-	$operation = explode('-', $_POST['name']);
-	$order = strtoupper($operation[0]);
-	$field_name = $operation[1];
+//accepting the value through ajax	
+$operation = explode('-', $_POST['name']);
+$order = strtoupper($operation[0]);
+$field_name = $operation[1];
 
-	$sort = new user();
-	$data = [];
-	$result  = $sort->sorting($field_name,$order);
-
-	if (mysqli_num_rows($result) > 0)
-    {
-      while ($row = mysqli_fetch_assoc($result))
-      {
-       	$data[] = $row;
-      }
-    }
-    else 
-    {
-        echo 'else';
-    }
+$sort = new data_filter();
+$data  = $sort->sorting($field_name,$order);
 
  echo json_encode($data);
  exit;
