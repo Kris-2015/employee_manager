9b757118cbc4e71d8784737a2d280867b72265ed
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-12 00:29:25 +0530

    changes in sort file

--- sort.php
+++ sort.php
@@ -6,7 +6,7 @@ require("DataFilter.php");
 //accepting the value through ajax	
 $operation = explode('-', $_POST['name']);
 $order = strtoupper($operation[0]);
-$field_name = $operation[1];
+$field_name = $operation[1]; 
 
 $sort = new data_filter();
 $data  = $sort->sorting($field_name,$order);
