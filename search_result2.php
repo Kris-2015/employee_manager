57844c8468451827567ebc9a88f0a695ed08dfa4
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-11 07:05:35 +0530

    modified the functionality in oops

--- search_result2.php
+++ search_result2.php
@@ -1,21 +1,14 @@
 <?php
 ini_set("display_error","1");
 session_start();                            
-include('user.php');
+include('DataFilter.php');
 
-       		$search_name = $_POST['name'];
-       		$search_email = $_POST['email'];
-       		
-            $obj = new user();
-            $data = [];
-            $result = $obj->search_user($search_name, $search_email);
-            if (mysqli_num_rows($result) > 0)
-            {
-              while ($row = mysqli_fetch_assoc($result))
-              {
-              	$data[] = $row;
-              }
-          	}
+$search_name = $_POST['name'];
+$search_email = $_POST['email'];
+     		
+$obj = new data_filter();
+$data = $obj->searching($search_name, $search_email);
 
 echo json_encode($data); 
 exit;
+?>
