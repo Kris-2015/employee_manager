0614293e1d79de57667bdcd5332ceebffa6beac6
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-13 15:51:18 +0530

    added isAllowed condition

--- control_permission.php
+++ control_permission.php
@@ -1,60 +1,63 @@
 <?php
-include("connection.php");
+include ("connection.php");
 
 /*
- * @author: mfsi_krishnadev
- * @purpose: access control
+* @author: mfsi_krishnadev
+* @purpose: access control
 */
-class ACL extends db_connection
+class role extends db_connection
+
 {
-	/*
-     * @access: public 
-     * @param: role_id
-     * @return: roles and privilege
-	*/
-   public function getrole($role_id)
-   {
-   	 $getrole_id = $role_id;
-     $data[] = [];
-     //fetching the role and privileges of user by employee_id
-   	 $get_auth =  "SELECT e.id, e.role_id, 
-         (SELECT r.role FROM role r WHERE r.role_id = e.role_id)as role,
-         (SELECT p.name FROM privilege p WHERE p.privilege_id = rrp.privilege_id)as privilege,
-         (SELECT res.resource FROM resource res WHERE res.resource_id = rrp.resource_id )as resource
-         FROM employee e
-         LEFT JOIN role_resource_privilege rrp ON e.role_id = rrp.role_id
-         LEFT JOIN privilege p ON rrp.privilege_id = p.privilege_id
-         LEFT JOIN resource res ON rrp.resource_id  = res.resource_id
-         WHERE e.id = 122";
+  /*
+  * @access: public
+  * @param: role_id
+  * @return: roles and privilege
+  */
+  public
 
-      //return mysqli_query($this->connect,$get_auth);
-      $query = mysqli_query($this->connect,$get_auth);
-      if(mysqli_num_rows($query) > 0)
+  function getrole($role_id)
+  {
+    $getrole_id = $role_id;
+
+    // fetching the role and privileges of user by employee_id
+
+    $get_auth = "SELECT res.resource, p.name AS privilege, r.role 
+      FROM role_resource_privilege rrp
+      LEFT JOIN role r ON rrp.role_id = r.role_id
+      LEFT JOIN privilege p ON rrp.privilege_id = p.privilege_id
+      LEFT JOIN resource res ON rrp.resource_id = res.resource_id
+      WHERE rrp.role_id =$getrole_id";
+    $query = mysqli_query($this->connect, $get_auth);
+    while ($row = $query->fetch_assoc())
+    {
+      $data[$row['resource']] = $row['privilege'];
+    }
+
+    $_SESSION['user_permission'] = $data;
+  }
+
+  /*
+  *function is checking the role, priviledges and resources
+  * @access: public
+  * @return:
+  */
+  public
+
+  function isResourceAllowed($resource)
+  {
+    $get_resource = trim("$resource", '/');
+    foreach($_SESSION['user_permission'] as $find_resource => $value)
+    {
+      if ($find_resource === $get_resource)
       {
-        while($row = mysqli_fetch_assoc($query))
-        {
-          $data[] = $row;
-        }
+        $get_privilege = $value;
+        return $get_privilege;
+        exit;
       }
-      $auth_data = $query;
-      echo "<pre>";
-      print_r($data);
-      exit;
-   }
-   /*
-    *function is checking the role, priviledges and resources 
-    * @access: public 
-    * @return:
-   */
-   public function getRolePrivilegeResource($role_id)
-   {
+    }
 
-   }
-   /**/
-   public function isResourceAllowed($resource)
-   {
-
-   }
-   /**/
+    return 0;
+  }
 }
+
 ?>
