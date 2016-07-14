4356ce333c60e9bb540cbec2920cece3a14d71be
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-14 17:09:40 +0530

    error handling codes

--- home.php
+++ home.php
@@ -45,9 +45,9 @@
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Welcome, <?php echo isset($_SESSION['user_name'])? $_SESSION['user_name'] : '' ; ?> <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="/profile.php">Profile</a></li>
-            <li><a href="/display.php"> Display </a></li>
-            <li><a href="/logout.php"> Logout </a></li>
+            <li><a href=" profile.php">Profile</a></li>
+            <li><a href=" display.php"> Display </a></li>
+            <li><a href=" logout.php"> Logout </a></li>
           </ul>
         </li>
 		 </ul>
