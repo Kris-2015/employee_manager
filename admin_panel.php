3d07e5860a6ccd37d3493556c7aaa52f497a477e
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-18 06:48:23 +0530

    change in admin panel file

--- admin_panel.php
+++ admin_panel.php
@@ -1,60 +1,47 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Admin Panel</title>
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Employee</a>
-    </div>
-  </div>
-</nav>
-
-<div class="container">
-  <div class="row">
-    <div class="col-xs-8">
-      <div class="panel panel-default">
-
-        <div class="panel-heading">
-         <p align="center"><strong>ADMIN PANEL</strong></p>
-        </div>
-        <div class="panel-body">
-          <form role="form">
-            <div class="form-group">
-              <label class="control-label col-xs-5">Administrator</label>
-               <div class="checkbox">
-                <label><input type="checkbox" value="">view</label>
-                <label><input type="checkbox" value="">insert</label>
-                <label><input type="checkbox" value="">update</label>
-                <label><input type="checkbox" value="">delete</label>
-                <label><input type="checkbox" value="">all</label>
-             </div>    
-           </div>
-           <div class="form-group">
-              <label class="control-label col-xs-5">User</label>
-              <div class="checkbox">
-                <label><input type="checkbox" value="">view</label>
-                <label><input type="checkbox" value="">insert</label>
-                <label><input type="checkbox" value="">update</label>
-                <label><input type="checkbox" value="">delete</label>
-                <label><input type="checkbox" value="">all</label>
-              </div>    
-           </div>
-            <div class="form-group" style="position:relative; left:35%;">
-              <div class="col-lg-12">
-                <input type = 'submit' name = 'submit' value = 'REGISTER' class = 'btn btn-info'>
-              </div>
+   <head>
+      <title>Admin Panel</title>
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <script type="text/javascript" src="../js/panel.js"></script>
+   </head>
+   <body>
+      <nav class="navbar navbar-inverse">
+         <div class="container-fluid">
+            <div class="navbar-header">
+               <a class="navbar-brand" href="#">Employee</a>
             </div>
-          </form>
-        </div>
+         </div>
+      </nav>
+      <div class="container">
+         <div class="row">
+            <div class="col-xs-8">
+               <div class="panel panel-default">
+                  <div class="panel-heading">
+                     <p align="center"><strong>ADMIN PANEL</strong></p>
+                  </div>
+                  <div class="panel-body">
+                     <form class="form-inline" role="form">
+                        <div class="form-group">
+                           <label for="role">Role:</label>
+                           <select class="form-control role" name="role">
+                              <option>--select--</option>
+                           </select>
+                        </div>
+                        <div class="form-group">
+                           <label for="resource">Resource:</label>
+                           <select class="form-control resource" name="resource">
+                           </select>
+                        </div>
+                        <div class="form-group privilege">
+                        </div>
+                     </form>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
-    </div>
-  </div>
-</div>
-</body>
+   </body>
 </html>
