9e2112345e3f8b343ef371813ec76d6ce9c306a9
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-19 07:54:36 +0530

    manage admin access

--- panel.php
+++ panel.php
@@ -3,9 +3,17 @@
 //instaintiatign the the ACL class
 $obj =  new ACL();
 
-if(isset($_POST['start'])==1)
+if($_POST['start']==1)
 {
 	$data = $obj->getrrp();
+}
+else if($_POST['start']==2)
+{
+	$data = $obj->getprivilege($_POST['role'],$_POST['resource']);
+}
+else if($_POST['start']==3)
+{
+	$data = $obj->setprivilege($_POST['role'],$_POST['resource'],$_POST['privilege'],$_POST['action']);
 }
  
 echo json_encode($data);
