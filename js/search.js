3613a636fff6a9b06626431d9d057023f9c4c80e
Kris-2015 <krishnadev.rnc@gmail.com>
2016-07-31 18:38:24 +0530

    change in search file

--- js/search.js
+++ js/search.js
@@ -14,17 +14,17 @@ $(document).ready(function()
             var arr_data = "";
             for (var key in data) {
                if (data.hasOwnProperty(key)) {
-                  arr_data += "<tr>";
-                  arr_data += "<td>" + data[key]["name"] + "</td>";
-                  arr_data += "<td>" + data[key]["gender"] + "</td>";
-                  arr_data += "<td>" + data[key]["dob"] + "</td>";
-                  arr_data += "<td>" + data[key]["email_id"] + "</td>";
-                  arr_data += "<td>" + data[key]["marital_status"] + "</td>";
-                  arr_data += "<td>" + data[key]["office"] + "</td>";
-                  arr_data += "<td>" + data[key]["residence"] + "</td>";
-                  arr_data += "<td>" + data[key]["communication"] + "</td>";
-                  arr_data += "<td>" + '<a href=" registration.php/?emp_id=' + data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href=" registration.php/?emp_id=' + data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + "</td>";
-                  arr_data += "</tr>";
+                  arr_data += '<tr>';
+                  arr_data += '<td><a href="#" class="u_name" data-github="'+data[key]["github_username"] +'" data-backdrop="static" data-keyboard="false" >' + data[key]["name"] + '</a></td>';
+                  arr_data += '<td>' + data[key]["gender"] + '</td>';
+                  arr_data += '<td>' + data[key]["dob"] + '</td>';
+                  arr_data += '<td>' + data[key]["email_id"] + '</td>';
+                  arr_data += '<td>' + data[key]["marital_status"] + '</td>';
+                  arr_data += '<td>' + data[key]["office"] + '</td>';
+                  arr_data += '<td>' + data[key]["residence"] + '</td>';
+                  arr_data += '<td>' + data[key]["communication"] + '</td>';
+                  arr_data += '<td>' + '<a href=" registration.php?emp_id=' + data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href=" registration.php?emp_id=' + data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + '</td>';
+                  arr_data += '</tr>';
                }
             }
 
@@ -53,24 +53,24 @@ $(document).ready(function()
             for (var key in result.data) {
 
                if (result.data.hasOwnProperty(key)) {
-                  show_data += "<tr>";
-                  show_data += "<td>" + result.data[key]["name"] + "</td>";
-                  show_data += "<td>" + result.data[key]["gender"] + "</td>";
-                  show_data += "<td>" + result.data[key]["dob"] + "</td>";
-                  show_data += "<td>" + result.data[key]["email_id"] + "</td>";
-                  show_data += "<td>" + result.data[key]["marital_status"] + "</td>";
-                  show_data += "<td>" + result.data[key]["office"] + "</td>";
-                  show_data += "<td>" + result.data[key]["residence"] + "</td>";
-                  show_data += "<td>" + result.data[key]["communication"] + "</td>";
+                  show_data += '<tr>';
+                  show_data += '<td><a href="#" class="u_name" data-github="' + result.data[key]["github_username"] +'"> ' + result.data[key]["name"] + '</a></td>';
+                  show_data += '<td>' + result.data[key]["gender"] + '</td>';
+                  show_data += '<td>' + result.data[key]["dob"] + '</td>';
+                  show_data += '<td>' + result.data[key]["email_id"] + '</td>';
+                  show_data += '<td>' + result.data[key]["marital_status"] + '</td>';
+                  show_data += '<td>' + result.data[key]["office"] + '</td>';
+                  show_data += '<td>' + result.data[key]["residence"] + '</td>';
+                  show_data += '<td>' + result.data[key]["communication"] + '</td>';
                   if (result.privileges == 'delete') {
-                     show_data += "<td>" + '<a href=" registration.php/?emp_id=' + result.data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>' + "</td>";
+                     show_data += '<td>' + '<a href=" registration.php?emp_id=' + result.data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>' + '</td>';
                   } else if (result.privileges == 'update') {
-                     show_data += "<td>" + '<a href=" registration.php/?emp_id=' + result.data[key]["id"] + '&action=update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + "</td>";
+                     show_data += '<td>' + '<a href=" registration.php?emp_id=' + result.data[key]["id"] + '&action=update"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + '</td>';
                   } else if (result.privileges == 'all') {
-                     show_data += "<td>" + '<a href=" registration.php/?emp_id=' + result.data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href=" registration.php/?emp_id=' + result.data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + "</td>";
+                     show_data += '<td>' + '<a href=" registration.php?emp_id=' + result.data[key]["id"] + '&action=delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>&nbsp;&nbsp;&nbsp;<a href=" registration.php?emp_id=' + result.data[key]["id"] + '&action=update"<span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>' + '</td>';
                   }
 
-                  show_data += "</tr>";
+                  show_data += '</tr>';
                }
             }
 
